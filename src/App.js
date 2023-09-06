43b18ea668e0a648cfd005581e381e7e8d2f8075
Pagrune <pagrune@orange.fr>
2023-09-06 14:45:28 +0200

    creation composants

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Presentation from'./components/presentation.js';
+
+function Etudes(){
+  return <ul>
+              <li>2021-2024 : BUT MMI </li>
+              <li>2019 : Bac S </li>
+          </ul>
+}
+function Experiences_pro(){
+  return  <div><h2>Mes expériences</h2>
+          <ul>
+              <li>2021 - 2022 : caissière</li>
+          </ul></div>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><Presentation/><Etudes/></>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/presentation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Presentation = () => {
+    return (
+        <div>
+            <div>GRUNEWALD Pauline</div>
+        </div>
+    );
+};
+
+export default Presentation;
